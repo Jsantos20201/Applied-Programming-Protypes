8d9375aa0f3984299c3c574ec126d9562697fb60
Jsantos20201 <72623931+Jsantos20201@users.noreply.github.com>
2023-06-16 10:58:03 -0600

    adding new program.

--- Media Player Protype/If and Else/Program.cs
+++ Media Player Protype/If and Else/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("The number is positive.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("The number is negative.");
+        }
+        else
+        {
+            Console.WriteLine("The number is zero.");
+        }
+    }
+}
+
